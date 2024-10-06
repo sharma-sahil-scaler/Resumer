965cb28b5ee867f9d214bec30c23d9e95c19f520
Sahil Sharma <sharma.sahil@scaler.com>
2024-10-06 16:54:45 +0530

    feat: basic backend setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
-from app.routes import users
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
-app.include_router(users.router)
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient("mongodb://db:27017")
+    app.mongodb = app.mongodb_client["fomo_resume_builder"]
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello! Ok"}
